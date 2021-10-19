7b68045a10e9843445798e1ccf74640e06cb00be
ShreeChandanReddy-9 <88181728+ShreeChandanReddy-9@users.noreply.github.com>
2021-10-19 14:08:47 +0530

    Create 1143. Longest Common Subsequence.cpp

---
+++ DynamicProgramming/1143. Longest Common Subsequence.cpp
@@ -0,0 +1,25 @@
+int longestCommonSubsequence(string text1, string text2) {
+        vector<vector<int> >dp(text1.size()+1,vector<int>(text2.size()+1,0));
+        for(int i=1;i<=text1.size();i++)
+            for(int j=1;j<=text2.size();j++)
+            {
+                if(text1[i-1]==text2[j-1])
+                {
+                    // dp[i][j]+=1;
+                    // if(i-1>=0)
+                    //     dp[i][j]=max(dp[i][j],dp[i-1][j]);
+                    // if(j-1>=0)
+                    //     dp[i][j]=max(dp[i][j],dp[i][j-1]);
+                    // if(i-1>=0&&j-1>0)
+                        dp[i][j]=max(dp[i][j],dp[i-1][j-1]+1);
+                }
+                else
+                {
+                    // if(i-1>=0)
+                    //     dp[i][j]=dp[i-1][j];
+                    // if(j-1>=0)
+                        dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        return dp[text1.size()][text2.size()];
+    }
